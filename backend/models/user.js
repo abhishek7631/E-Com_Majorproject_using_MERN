f321aa7a9c880d75bc714ba6737cd8955aa99d2d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2025-01-20 21:37:34 +0530

    create user models

---
+++ backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    role: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
