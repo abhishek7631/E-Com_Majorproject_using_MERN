51b7709db1a0799602cea7bf4c0460410fa2d10d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2025-02-03 22:12:51 +0530

    implement about page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            Any query and info about prodduct feel free to call anytime. We 24X7
+            Any query and info about product feel free to call anytime. We 24X7
             available
           </p>
           <p className="mt-3">
