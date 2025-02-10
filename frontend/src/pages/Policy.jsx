5f8f0284d6a14c8fedcf269b9c744963faa7d29c
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2025-02-10 18:48:07 +0530

    privacy policy page modified

--- frontend/src/pages/Policy.jsx
+++ frontend/src/pages/Policy.jsx
@@ -13,13 +13,18 @@ const Policy = () => {
           />
         </div>
         <div className="col-md-4">
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
-          <p>add privacy policy</p>
+          <p>
+            We value your privacy. Any personal information collected on this
+            website, such as name, email, or browsing data, is used only to
+            enhance your experience. We do not sell or share your data with
+            third parties except as required by law. Cookies may be used to
+            improve functionality and analytics. By using this site, you agree
+            to our privacy practices.
+          </p>
+          <p>
+            For more details, contact us at
+            <b> www.help@ecommerceapp.com</b>
+          </p>
         </div>
       </div>
     </Layout>
